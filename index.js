9ff779c669209a898f59a13b364169da074a95ad
evan <aspaasevan@gmail.com>
2020-02-25 15:29:31 -0800

    counterMaker complete and passing

--- index.js
+++ index.js
@@ -280,9 +280,10 @@ function tallyUpDonations(runners) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
-  function counter() {
-    ++count
+  let count = 0;
+  
+  return function counter() {
+    count ++;
   }
   // BROKEN CODE ENDS
 }
